52b14b60f6d9568efbae10061407db2b4110864e
Sylvain Bruyat <sylvain.bruyat@orange.fr>
2022-05-03 16:26:59 +0200

    Ajout de la fonction d'ajout d'un produit dans le panier

--- front/js/product.js
+++ front/js/product.js
@@ -60,10 +60,10 @@ function addProductColorsToPage(colors) {
     }
 }
 
-let message = document.createElement("div");
+let quantityErrorMessage = document.createElement("div");
 document
         .querySelector("div.item__content__settings__quantity")
-        .appendChild(message);
+        .appendChild(quantityErrorMessage);
 
 let quantity = document.getElementById("quantity");
 quantity.addEventListener("change", function(event) {
@@ -76,16 +76,94 @@ quantity.addEventListener("change", function(event) {
     else {
         removeErrorMessage();
     }
-})
+});
 
 function removeErrorMessage() {
-    message.textContent = "";
+    quantityErrorMessage.textContent = ""; //Action sur une variable hors scope. A améliorer ?
 }
 
 function displayNotANumberErrorMessage() {
-    message.textContent = "Veuillez entrer un nombre entier";
+    quantityErrorMessage.textContent = "Veuillez entrer un nombre entier"; //Action sur une variable hors scope. A améliorer ?
 }
 
 function displayValueRangeErrorMessage() {
-    message.textContent = "Veuillez entrer une quantité entre 1 et 100";
+    quantityErrorMessage.textContent = "Veuillez entrer une quantité entre 1 et 100"; //Action sur une variable hors scope. A améliorer ?
+}
+
+let addToCartButton = document.getElementById("addToCart");
+addToCartButton.addEventListener("click", function(){
+    let productInfo = readFormInfo();
+    let formIsValid = validateProductForm(productInfo);
+    if (formIsValid) {
+        let cart = addProductToCart(productInfo);
+        saveCart(cart);
+    }
+});
+
+function validateProductForm(product) {
+    if (product.color === "") {
+        alert("Votre produit n'a pas pu être ajouté au panier car vous n'avez pas choisi de couleur");
+        return false;
+    }
+    else if (product.quantity === "" || product.quantity < 1 || product.quantity > 100 || parseInt(product.quantity) != parseFloat(product.quantity)) {
+        alert("Votre produit n'a pas pu être ajouté au panier. La quantité doit être un nombre entier entre 1 et 100.");
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
+function readFormInfo() {
+    let id = productID;
+    let color = document.getElementById("colors").value;
+    let quantity = document.getElementById("quantity").value;
+
+    return {id, color, quantity};
+}
+
+function addProductToCart(product) {
+    let cart = getCart();
+    let productAlreadyInCart = checkIfProductAlreadyInCart(product, cart);
+    if (productAlreadyInCart) {
+        cart = updateProductQuantity(product, cart);
+    }
+    else {
+        cart.push(product);
+    }
+    
+    return cart;
+}
+
+function getCart() {
+    let cart = localStorage.getItem("cart");
+    if (cart == null) {
+        return [];
+    }
+    else {
+        return JSON.parse(cart);
+    }
+}
+
+function checkIfProductAlreadyInCart(product, cart) {
+    for (let item of cart) {
+        if (item.id === product.id && item.color === product.color) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function updateProductQuantity(product, cart) {
+    for (let item of cart) {
+        if (item.id === product.id && item.color === product.color) { /*** Code répété ! A refactoriser/améliorer ***/
+            let sum = parseInt(item.quantity) + parseInt(product.quantity);
+            item.quantity = sum.toString();
+            return cart;
+        }
+    }
+}
+
+function saveCart(cart) {
+    localStorage.setItem("cart", JSON.stringify(cart));
 }
