48c6e107ab9ff2804263a21faa1144f8abb11932
Sylvain Bruyat <sylvain.bruyat@orange.fr>
2022-05-16 12:17:38 +0200

    script.js : lecture DOM sortie de la boucle for pour optimiser les performances

--- front/js/script.js
+++ front/js/script.js
@@ -4,22 +4,21 @@ fetch("http://localhost:3000/api/products/")
             return response.json();
     })
     .then(displayAllProducts)
-    .catch(function() { // Voir pour remplacer par console.error, vérifier quel message est renvoyé par l'API
+    .catch(function() { // Voir pour remplacer par console.log(error), vérifier quel message est renvoyé par l'API
         console.log("Something went wrong with the request to get all the products from the API");
     });
 
 function displayAllProducts(products){
+    let items = document.getElementById("items")
     for (let product of products) {
-        document
-            .getElementById("items")
-            .innerHTML += `
-                <a href="./product.html?id=${product._id}">
-                    <article>
-                        <img src="${product.imageUrl}" alt="${product.altTxt}">
-                        <h3 class="productName">${product.name}</h3>
-                        <p class="productDescription">${product.description}</p>
-                    </article>
-                </a>
-            `;
+        items.innerHTML += `
+            <a href="./product.html?id=${product._id}">
+                <article>
+                    <img src="${product.imageUrl}" alt="${product.altTxt}">
+                    <h3 class="productName">${product.name}</h3>
+                    <p class="productDescription">${product.description}</p>
+                </article>
+            </a>
+        `;
     }
 };
