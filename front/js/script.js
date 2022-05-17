533c15468e36bec6a29936ec08609c7743214f2b
Sylvain Bruyat <sylvain.bruyat@orange.fr>
2022-05-17 16:26:49 +0200

    script.js : rétablissement de la requête API fonctionnelle suite à la fin des tests

--- front/js/script.js
+++ front/js/script.js
@@ -1,4 +1,4 @@
-fetch("http://localhost:3000/api/product/")
+fetch("http://localhost:3000/api/products/")
     .then(function(response) {
         if (response.ok)
             return response.json();
